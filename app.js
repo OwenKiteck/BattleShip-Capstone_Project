cd6899f80b03a5a156fcff590f72bf33d057beb8
Student <owen.kiteck@stu.barren.kyschools.us>
2022-04-29 15:53:10 -0500

    Added some styling. Created app.js, selected every square

---
+++ app.js
@@ -0,0 +1,232 @@
+// Selectors for each square in opponents table
+{
+    const OA1 = document.getElementById(`opponent A1`);
+    const OA2 = document.getElementById(`opponent A2`);
+    const OA3 = document.getElementById(`opponent A3`);
+    const OA4 = document.getElementById(`opponent A4`);
+    const OA5 = document.getElementById(`opponent A5`);
+    const OA6 = document.getElementById(`opponent A6`);
+    const OA7 = document.getElementById(`opponent A7`);
+    const OA8 = document.getElementById(`opponent A8`);
+    const OA9 = document.getElementById(`opponent A9`);
+    const OA0 = document.getElementById(`opponent A0`);
+
+    const OB1 = document.getElementById(`opponent B1`);
+    const OB2 = document.getElementById(`opponent B2`);
+    const OB3 = document.getElementById(`opponent B3`);
+    const OB4 = document.getElementById(`opponent B4`);
+    const OB5 = document.getElementById(`opponent B5`);
+    const OB6 = document.getElementById(`opponent B6`);
+    const OB7 = document.getElementById(`opponent B7`);
+    const OB8 = document.getElementById(`opponent B8`);
+    const OB9 = document.getElementById(`opponent B9`);
+    const OB0 = document.getElementById(`opponent B0`);
+
+    const OC1 = document.getElementById(`opponent C1`);
+    const OC2 = document.getElementById(`opponent C2`);
+    const OC3 = document.getElementById(`opponent C3`);
+    const OC4 = document.getElementById(`opponent C4`);
+    const OC5 = document.getElementById(`opponent C5`);
+    const OC6 = document.getElementById(`opponent C6`);
+    const OC7 = document.getElementById(`opponent C7`);
+    const OC8 = document.getElementById(`opponent C8`);
+    const OC9 = document.getElementById(`opponent C9`);
+    const OC0 = document.getElementById(`opponent C0`);
+
+    const OD1 = document.getElementById(`opponent D1`);
+    const OD2 = document.getElementById(`opponent D2`);
+    const OD3 = document.getElementById(`opponent D3`);
+    const OD4 = document.getElementById(`opponent D4`);
+    const OD5 = document.getElementById(`opponent D5`);
+    const OD6 = document.getElementById(`opponent D6`);
+    const OD7 = document.getElementById(`opponent D7`);
+    const OD8 = document.getElementById(`opponent D8`);
+    const OD9 = document.getElementById(`opponent D9`);
+    const OD0 = document.getElementById(`opponent D0`);
+
+    const OE1 = document.getElementById(`opponent E1`);
+    const OE2 = document.getElementById(`opponent E2`);
+    const OE3 = document.getElementById(`opponent E3`);
+    const OE4 = document.getElementById(`opponent E4`);
+    const OE5 = document.getElementById(`opponent E5`);
+    const OE6 = document.getElementById(`opponent E6`);
+    const OE7 = document.getElementById(`opponent E7`);
+    const OE8 = document.getElementById(`opponent E8`);
+    const OE9 = document.getElementById(`opponent E9`);
+    const OE0 = document.getElementById(`opponent E0`);
+
+    const OF1 = document.getElementById(`opponent F1`);
+    const OF2 = document.getElementById(`opponent F2`);
+    const OF3 = document.getElementById(`opponent F3`);
+    const OF4 = document.getElementById(`opponent F4`);
+    const OF5 = document.getElementById(`opponent F5`);
+    const OF6 = document.getElementById(`opponent F6`);
+    const OF7 = document.getElementById(`opponent F7`);
+    const OF8 = document.getElementById(`opponent F8`);
+    const OF9 = document.getElementById(`opponent F9`);
+    const OF0 = document.getElementById(`opponent F0`);
+
+    const OG1 = document.getElementById(`opponent G1`);
+    const OG2 = document.getElementById(`opponent G2`);
+    const OG3 = document.getElementById(`opponent G3`);
+    const OG4 = document.getElementById(`opponent G4`);
+    const OG5 = document.getElementById(`opponent G5`);
+    const OG6 = document.getElementById(`opponent G6`);
+    const OG7 = document.getElementById(`opponent G7`);
+    const OG8 = document.getElementById(`opponent G8`);
+    const OG9 = document.getElementById(`opponent G9`);
+    const OG0 = document.getElementById(`opponent G0`);
+
+    const OH1 = document.getElementById(`opponent H1`);
+    const OH2 = document.getElementById(`opponent H2`);
+    const OH3 = document.getElementById(`opponent H3`);
+    const OH4 = document.getElementById(`opponent H4`);
+    const OH5 = document.getElementById(`opponent H5`);
+    const OH6 = document.getElementById(`opponent H6`);
+    const OH7 = document.getElementById(`opponent H7`);
+    const OH8 = document.getElementById(`opponent H8`);
+    const OH9 = document.getElementById(`opponent H9`);
+    const OH0 = document.getElementById(`opponent H0`);
+
+    const OI1 = document.getElementById(`opponent I1`);
+    const OI2 = document.getElementById(`opponent I2`);
+    const OI3 = document.getElementById(`opponent I3`);
+    const OI4 = document.getElementById(`opponent I4`);
+    const OI5 = document.getElementById(`opponent I5`);
+    const OI6 = document.getElementById(`opponent I6`);
+    const OI7 = document.getElementById(`opponent I7`);
+    const OI8 = document.getElementById(`opponent I8`);
+    const OI9 = document.getElementById(`opponent I9`);
+    const OI0 = document.getElementById(`opponent I0`);
+
+    const OJ1 = document.getElementById(`opponent J1`);
+    const OJ2 = document.getElementById(`opponent J2`);
+    const OJ3 = document.getElementById(`opponent J3`);
+    const OJ4 = document.getElementById(`opponent J4`);
+    const OJ5 = document.getElementById(`opponent J5`);
+    const OJ6 = document.getElementById(`opponent J6`);
+    const OJ7 = document.getElementById(`opponent J7`);
+    const OJ8 = document.getElementById(`opponent J8`);
+    const OJ9 = document.getElementById(`opponent J9`);
+    const OJ0 = document.getElementById(`opponent J0`);
+}
+
+// Selectors for each square in your table
+{
+    const MA1 = document.getElementById(`my-board A1`);
+    const MA2 = document.getElementById(`my-board A2`);
+    const MA3 = document.getElementById(`my-board A3`);
+    const MA4 = document.getElementById(`my-board A4`);
+    const MA5 = document.getElementById(`my-board A5`);
+    const MA6 = document.getElementById(`my-board A6`);
+    const MA7 = document.getElementById(`my-board A7`);
+    const MA8 = document.getElementById(`my-board A8`);
+    const MA9 = document.getElementById(`my-board A9`);
+    const MA0 = document.getElementById(`my-board A0`);
+
+    const MB1 = document.getElementById(`my-board B1`);
+    const MB2 = document.getElementById(`my-board B2`);
+    const MB3 = document.getElementById(`my-board B3`);
+    const MB4 = document.getElementById(`my-board B4`);
+    const MB5 = document.getElementById(`my-board B5`);
+    const MB6 = document.getElementById(`my-board B6`);
+    const MB7 = document.getElementById(`my-board B7`);
+    const MB8 = document.getElementById(`my-board B8`);
+    const MB9 = document.getElementById(`my-board B9`);
+    const MB0 = document.getElementById(`my-board B0`);
+
+    const MC1 = document.getElementById(`my-board C1`);
+    const MC2 = document.getElementById(`my-board C2`);
+    const MC3 = document.getElementById(`my-board C3`);
+    const MC4 = document.getElementById(`my-board C4`);
+    const MC5 = document.getElementById(`my-board C5`);
+    const MC6 = document.getElementById(`my-board C6`);
+    const MC7 = document.getElementById(`my-board C7`);
+    const MC8 = document.getElementById(`my-board C8`);
+    const MC9 = document.getElementById(`my-board C9`);
+    const MC0 = document.getElementById(`my-board C0`);
+
+    const MD1 = document.getElementById(`my-board D1`);
+    const MD2 = document.getElementById(`my-board D2`);
+    const MD3 = document.getElementById(`my-board D3`);
+    const MD4 = document.getElementById(`my-board D4`);
+    const MD5 = document.getElementById(`my-board D5`);
+    const MD6 = document.getElementById(`my-board D6`);
+    const MD7 = document.getElementById(`my-board D7`);
+    const MD8 = document.getElementById(`my-board D8`);
+    const MD9 = document.getElementById(`my-board D9`);
+    const MD0 = document.getElementById(`my-board D0`);
+
+    const ME1 = document.getElementById(`my-board E1`);
+    const ME2 = document.getElementById(`my-board E2`);
+    const ME3 = document.getElementById(`my-board E3`);
+    const ME4 = document.getElementById(`my-board E4`);
+    const ME5 = document.getElementById(`my-board E5`);
+    const ME6 = document.getElementById(`my-board E6`);
+    const ME7 = document.getElementById(`my-board E7`);
+    const ME8 = document.getElementById(`my-board E8`);
+    const ME9 = document.getElementById(`my-board E9`);
+    const ME0 = document.getElementById(`my-board E0`);
+
+    const MF1 = document.getElementById(`my-board F1`);
+    const MF2 = document.getElementById(`my-board F2`);
+    const MF3 = document.getElementById(`my-board F3`);
+    const MF4 = document.getElementById(`my-board F4`);
+    const MF5 = document.getElementById(`my-board F5`);
+    const MF6 = document.getElementById(`my-board F6`);
+    const MF7 = document.getElementById(`my-board F7`);
+    const MF8 = document.getElementById(`my-board F8`);
+    const MF9 = document.getElementById(`my-board F9`);
+    const MF0 = document.getElementById(`my-board F0`);
+
+    const MG1 = document.getElementById(`my-board G1`);
+    const MG2 = document.getElementById(`my-board G2`);
+    const MG3 = document.getElementById(`my-board G3`);
+    const MG4 = document.getElementById(`my-board G4`);
+    const MG5 = document.getElementById(`my-board G5`);
+    const MG6 = document.getElementById(`my-board G6`);
+    const MG7 = document.getElementById(`my-board G7`);
+    const MG8 = document.getElementById(`my-board G8`);
+    const MG9 = document.getElementById(`my-board G9`);
+    const MG0 = document.getElementById(`my-board G0`);
+
+    const MH1 = document.getElementById(`my-board H1`);
+    const MH2 = document.getElementById(`my-board H2`);
+    const MH3 = document.getElementById(`my-board H3`);
+    const MH4 = document.getElementById(`my-board H4`);
+    const MH5 = document.getElementById(`my-board H5`);
+    const MH6 = document.getElementById(`my-board H6`);
+    const MH7 = document.getElementById(`my-board H7`);
+    const MH8 = document.getElementById(`my-board H8`);
+    const MH9 = document.getElementById(`my-board H9`);
+    const MH0 = document.getElementById(`my-board H0`);
+
+    const MI1 = document.getElementById(`my-board I1`);
+    const MI2 = document.getElementById(`my-board I2`);
+    const MI3 = document.getElementById(`my-board I3`);
+    const MI4 = document.getElementById(`my-board I4`);
+    const MI5 = document.getElementById(`my-board I5`);
+    const MI6 = document.getElementById(`my-board I6`);
+    const MI7 = document.getElementById(`my-board I7`);
+    const MI8 = document.getElementById(`my-board I8`);
+    const MI9 = document.getElementById(`my-board I9`);
+    const MI0 = document.getElementById(`my-board I0`);
+
+    const MJ1 = document.getElementById(`my-board J1`);
+    const MJ2 = document.getElementById(`my-board J2`);
+    const MJ3 = document.getElementById(`my-board J3`);
+    const MJ4 = document.getElementById(`my-board J4`);
+    const MJ5 = document.getElementById(`my-board J5`);
+    const MJ6 = document.getElementById(`my-board J6`);
+    const MJ7 = document.getElementById(`my-board J7`);
+    const MJ8 = document.getElementById(`my-board J8`);
+    const MJ9 = document.getElementById(`my-board J9`);
+    const MJ0 = document.getElementById(`my-board J0`);
+}
+
+OD6.addEventListener(`click`, () => {
+    alert(`Clicked`)
+}); 
+MA2.addEventListener(`click`, () => {
+    alert(`Clicked`)
+}); 
